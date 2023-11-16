4557da546d5206eddb2fdca905de02deada0ceb3
Hin <tonghohin77@gmail.com>
2023-11-15 22:59:41 -0400

    Remove address from contact page and update icon spacing.

--- app/(public)/(main)/contact/page.tsx
+++ app/(public)/(main)/contact/page.tsx
@@ -1,22 +1,30 @@
-import { IconBrandWhatsapp, IconClock, IconHome, IconMapPin, IconPhone } from "@tabler/icons-react";
+import { IconBrandWhatsapp, IconClock, IconHome, IconPhone } from "@tabler/icons-react";
 
 export default function Contact() {
     return (
         <section className="bg-yellow-400 rounded p-4 flex flex-col gap-4 items-center">
             <div className="flex items-center gap-2">
-                <IconHome size={24} />
+                <div>
+                    <IconHome size={24} />
+                </div>
                 <span>葵涌 梨木道32-50號 金運工業大廈 第二座 Foodie City</span>
             </div>
             <div className="flex items-center gap-2">
-                <IconPhone size={24} />
+                <div>
+                    <IconPhone size={24} />
+                </div>
                 <span>+852 9558 2500</span>
             </div>
             <div className="flex items-center gap-2">
-                <IconBrandWhatsapp size={24} />
+                <div>
+                    <IconBrandWhatsapp size={24} />
+                </div>
                 <span>+852 6343 9624</span>
             </div>
             <div className="flex items-center gap-2">
-                <IconClock size={24} />
+                <div>
+                    <IconClock size={24} />
+                </div>
                 <span>11:00 a.m. - 9:00 p.m.</span>
             </div>
         </section>
